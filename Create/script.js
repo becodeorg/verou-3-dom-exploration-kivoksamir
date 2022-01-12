b44c23d91f6e202892bbac1583a6789e2812f22c
kivoksamir <upahmadalkordee@gmail.com>
2022-01-12 14:13:50 +0100

    Add create

--- Create/script.js
+++ Create/script.js
@@ -29,11 +29,11 @@ for (var i = 0; i < learners.length; i++){
 
     const section_1=document.createElement("section");
     document.body.appendChild(section_1);
-    section_1.innerHTML=(learners_index);
+    section_1.innerHTML=(learners_index); // father include son
 
     console.log(learners_index);
     
-    artical_1.append(section_1);
+    artical_1.append(section_1); // father include son
     section_1.style.backgroundColor="#" + Math.floor(Math.random()*5000);
 
 
