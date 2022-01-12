f0404a16fe1c966262548f511ecb9585e47474d4
kivoksamir <upahmadalkordee@gmail.com>
2022-01-12 14:16:01 +0100

    Add chapter 2 as regular folder

---
+++ 2.Selectors/script.js
@@ -0,0 +1,75 @@
+
+
+
+
+const important_items = document.querySelectorAll(".important")
+console.log(important_items);
+
+
+/* add attribute */
+important_items.forEach((element)=>{
+
+    element.setAttribute("title","This is an important item")
+
+});
+
+console.log(important_items);
+
+
+
+
+/* loop through , add classes to IMG */
+
+const images=document.querySelectorAll("img");
+console.log(images);
+
+images.forEach((img)=>{
+    
+    if(img.className != "important"){
+
+        img.style.display="none";
+
+}})
+
+
+
+/* loop through P .. Show the paragraph content */
+/*show the className  */
+
+const paragraph = document.querySelectorAll("p");
+console.log(paragraph);
+paragraph.forEach((content)=>{console.log(content.innerHTML +"  "+ content.className)});
+
+
+
+const parag_content = document.getElementsByTagName("p");
+
+for (i=0; i < paragraph.length ; i++){
+
+    function randomcolor(){
+        let r = Math.random() *255 ; 
+        let g = Math.random() *255 ;
+        let b = Math.random() *255 ; 
+    var bgcolor = "rgb(" + r + "," + g + "," + b + ")";
+    paragraph[i].style.color=bgcolor;
+
+    }
+    randomcolor();
+}
+
+
+  
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
