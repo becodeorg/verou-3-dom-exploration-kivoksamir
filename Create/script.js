312e89dd90001bbc8c47a3867967fb57d6997ecd
kivoksamir <upahmadalkordee@gmail.com>
2022-01-12 13:29:18 +0100

    creat excer

---
+++ Create/script.js
@@ -0,0 +1,63 @@
+//get all learner .. 
+//create a section .. 
+//create an artical .. 
+//genertate a random color 
+//do it per lerner .. 
+//append to artical .. 
+
+
+
+
+
+
+
+
+const learners=[
+    "jordy",
+    "ahmad",
+    "michail",
+    "danty"
+];
+
+const artical_1=document.createElement("artical")
+document.body.appendChild(artical_1);
+
+
+
+for (var i = 0; i < learners.length; i++){
+    const learners_index=learners[i];
+
+    const section_1=document.createElement("section");
+    document.body.appendChild(section_1);
+    section_1.innerHTML=(learners_index);
+
+    console.log(learners_index);
+    
+    artical_1.append(section_1);
+    section_1.style.backgroundColor="#" + Math.floor(Math.random()*5000);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
