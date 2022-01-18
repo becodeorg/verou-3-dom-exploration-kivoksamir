4e4840278307f2ff29837048f3a5447023476688
kivoksamir <upahmadalkordee@gmail.com>
2022-01-18 16:40:48 +0100

    showing section of age >18

--- Forms/script.js
+++ Forms/script.js
@@ -5,3 +5,20 @@ first_input.addEventListener('keyup' , ()=>{
     let display_1=document.querySelector("#display-firstname");
     display_1.innerText=enterd_text;
 })
+
+
+
+const the_age =document.querySelector("#age");
+const section_1=document.querySelector("#a-hard-truth");
+the_age.addEventListener('keyup',()=>{
+
+    input_value=the_age.value; // must be inside the if loop
+
+    
+    if(input_value > 18 ){
+    section_1.style.visibility="visible";
+    }else{
+    section_1.style.visibility = "hidden";
+    }
+
+});
